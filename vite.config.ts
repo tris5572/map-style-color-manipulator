8338e3863889f3eec52f28a37bb7f989800c2c45
tris <nexus4fortune@gmail.com>
2024-05-14 05:40:40 +0900

    chore: GitHub Pages へのデプロイ設定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  // GitHub Pages へデプロイする際のフォルダ名として、リポジトリ名を設定
+  const base =
+    mode === "production" ? "/map-style-color-manipulator/" : undefined;
+
+  return {
+    base,
+    plugins: [react()],
+  };
+});
